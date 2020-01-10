84990e6c886850cb91c80d6b894d22967bba1970
Laura Kissack <lkissack@uvic.ca>
2020-01-10 11:09:18 -0800

    add plans for next steps

--- Assignment 1/AddNoise.m
+++ Assignment 1/AddNoise.m
@@ -2,8 +2,14 @@ function [] = AddNoise(DicomAddress,NoiseStats)
 %NoiseStats = [Mean, Std]
 
 originalImage = dicomread(DicomAddress);
+info = dicominfo(DicomAddress)
 firstSlice = originalImage(:,:,:,1);
+
+%for testing purposes
 imshow(firstSlice, [])
+
+%replace negative values in slice with 0
+
 %add gaussian noise to first slice of dicom file
 %do not use MATLAB provided imnoise
 

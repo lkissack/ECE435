e440d4f850021f7eb6b6eab2d45ffd82758bd886
lkissack <kissack.laura@gmail.com>
2020-01-12 09:22:13 -0800

    Normalize between [0,1], save as .png

--- Assignment 1/AddNoise.m
+++ Assignment 1/AddNoise.m
@@ -5,19 +5,20 @@ image = dicomread(DicomAddress);
 %info = dicominfo(DicomAddress)
 
 %what are the dimensions of the image?
-[rows, cols, samples, slices] = size(image);
+%[rows, cols, samples, slices] = size(image);
 
 firstSlice = image(:,:,:,1);
 
 %replace all negative entries with 0
 firstSlice = max(firstSlice, 0);
 
-%Save as the original
-
+%Normalize into [0,1] range and save as original .png
+original = mat2gray(firstSlice);
+imwrite(original,'Testfile.png');
 
 %for testing purposes
-imshow(firstSlice, [])
-
+%imshow(firstSlice, [])
+imshow(original, [])
 
 %replace negative values in slice with 0
 

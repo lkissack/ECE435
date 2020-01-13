f63ba7eb4beb00176cfef8e1ca1908ad07534ef6
Laura Kissack <lkissack@uvic.ca>
2020-01-13 11:10:01 -0800

    Subplot images together

--- Assignment 1/AddNoise.m
+++ Assignment 1/AddNoise.m
@@ -14,25 +14,40 @@ firstSlice = image(:,:,:,1);
 %replace all negative values in slice with 0
 firstSlice = max(firstSlice, 0);
 
-%Normalize into [0,1] range and save as original .png
-original = mat2gray(firstSlice);
-imwrite(original,'Testfile.png');
+original = firstSlice;
+
+
 
 %for testing purposes
-%imshow(firstSlice, [])
+%Im1 = imshow(firstSlice, []);
 %imshow(original, [])
 
+%random returns float
 gNoise = random('Normal', Mean, Std,[rows,cols]);
+
+%if int is to be used instead
+%M(1:rows, 1:cols)= Mean;
+%gNoise = M + Std.*randn([rows,cols]);
 
 %not sure if this should be additive or multiplicative?
 gImg = original + gNoise;
 %do not use MATLAB provided imnoise
 
-imshow(gImg,[])
+%Im2 = imshow(gImg,[]);
+subplot(1,2,1);
+imshow(firstSlice, []);
+title("Original");
+subplot(1,2,2);
+imshow(gImg,[]);
+title({"Gaussian Noise";sprintf("Mean = %d Std = %d", Mean,Std)});
+
 %save new images as OrgImg.png and GaussianNoise.png in SAME folder as
 %original
+% final = mat2gray(gImg);
+% imwrite(final,"FinalTestfile.png")
 
-
-
+%Normalize into [0,1] range and save as original .png
+original = mat2gray(firstSlice);%converts to double
+imwrite(original,'Testfile.png');
 end
 

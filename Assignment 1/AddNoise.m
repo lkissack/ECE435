c7cb3283995277a169b69cda732c6c34e5466881
lkissack <kissack.laura@gmail.com>
2020-01-13 06:29:46 -0800

    use random for gaussian noise

--- Assignment 1/AddNoise.m
+++ Assignment 1/AddNoise.m
@@ -1,15 +1,17 @@
 function [] = AddNoise(DicomAddress,NoiseStats)
-%NoiseStats = [Mean, Std]
+
+Mean = NoiseStats(1);
+Std = NoiseStats(2);
 
 image = dicomread(DicomAddress);
 %info = dicominfo(DicomAddress)
 
 %what are the dimensions of the image?
-%[rows, cols, samples, slices] = size(image);
+[rows, cols, samples, slices] = size(image);
 
 firstSlice = image(:,:,:,1);
 
-%replace all negative entries with 0
+%replace all negative values in slice with 0
 firstSlice = max(firstSlice, 0);
 
 %Normalize into [0,1] range and save as original .png
@@ -18,14 +20,19 @@ imwrite(original,'Testfile.png');
 
 %for testing purposes
 %imshow(firstSlice, [])
-imshow(original, [])
+%imshow(original, [])
 
-%replace negative values in slice with 0
+gNoise = random('Normal', Mean, Std,[rows,cols]);
 
-%add gaussian noise to first slice of dicom file
+%not sure if this should be additive or multiplicative?
+gImg = original + gNoise;
 %do not use MATLAB provided imnoise
 
+imshow(gImg,[])
 %save new images as OrgImg.png and GaussianNoise.png in SAME folder as
 %original
+
+
+
 end
 

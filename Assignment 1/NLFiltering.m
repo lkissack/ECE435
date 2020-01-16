32047b7b25285c2c3aa74752cc83d192af88553b
lkissack <kissack.laura@gmail.com>
2020-01-15 18:30:59 -0800

    Create fn for q4 d

---
+++ Assignment 1/NLFiltering.m
@@ -0,0 +1,12 @@
+function [Iout] = NLFiltering(OrgImgAdd,n)
+%use median filter 
+%nl filter is sliding neighbourhoodfilter
+%colfilt divides into regions
+
+%load original image
+original = imread(OrgImgAdd);
+original = mat2gray(original);
+
+
+end
+

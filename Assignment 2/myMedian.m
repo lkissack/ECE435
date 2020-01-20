45c0a82d853cac701f7ebca3548892c7cf4743d8
Laura Kissack <lkissack@uvic.ca>
2020-01-20 10:48:27 -0800

    Fix myMedian function to filter each colour individually

--- Assignment 2/myMedian.m
+++ Assignment 2/myMedian.m
@@ -5,19 +5,29 @@ function [denoised] = myMedian(img,wsize)
 %image padded by repeating border elements
 [rows, cols, dim] = size(img);
 padding = wsize - 1;
-padded = padarray(img, [(wsize -1), (wsize -1)],'replicate', 'both')
+padded = padarray(img, [(wsize -1), (wsize -1)],'replicate', 'both');
 
-%generate kernel - DOES NOT work
+%generate kernel - Takes the median for a single colour (2d only)
 med = @(x) median(x(:));
-%do not use medfilt2 function
 
-%run into issues due to dimensions of the image.
-filtered = nlfilter(padded, [wsize, wsize], med);
+%allocate and specify to use uint8 (otherwise result is white)
+filtered = zeros(rows + 2*padding, cols +2*padding, dim, 'uint8');
+
+for colour = 1:dim
+    filtered(:,:,colour) = nlfilter( padded(:,:,colour), [wsize, wsize], med);
+end
+
 denoised = filtered(padding +1: rows+ padding, padding + 1:padding +cols, :);
-
+    
 %save images as .png
 imwrite(img, '1-Noisy.png');
 imwrite(denoised, '2-Denoised.png');
+
+%for testing purposes
+% subplot(1,2,1);
+% imshow(img, []);
+% subplot(1,2,2);
+% imshow(denoised, []);
 
 end
 

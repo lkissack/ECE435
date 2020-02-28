b6c5fd23272cf7a50c8db267272a960bb1ca937b
Laura Kissack <lkissack@uvic.ca>
2020-02-28 14:38:55 -0800

    Prep for zipping

--- Assignment 3/SnakesTemp_Solution.m
+++ Assignment 3/SnakesTemp_Solution.m
@@ -2,20 +2,26 @@ clear all
 clc
 
 % Minimization Parameters
+
+alpha = 1;
+gamma = 10;
+beta = 0.0000;
 max_itr      = 100;
-GaussWinSize = 5;
-GradT        = 0.01;%Gradient Threshold
+GaussWinSize = 5;%Arbitrarily chosen
+GradT        = 1;%Gradient Threshold
 
 % Loading the image and converting that to a grey-level image if needed
 original  = imread('CTImage.png');
 [rows, cols, dim] = size(original);
-if dim == 3 % check the solution key to see if this is the correct way to do this
+if dim == 3 
     original = rgb2gray(original);
 end
 imshow(original,[]);
-% 1. Denoising by Gaussian filter - what should std be?
+% 1. Denoising by Gaussian filter 
+% Assuming std is 1
 std = 1;
-gk = myGaussian(GaussWinSize,std);%check to make sure that this function is correct
+gk = myGaussian(GaussWinSize,std);% Assumes that file provided for
+% Assignment 2 is correct
 denoised = imfilter(original,gk,'conv');
 % 
 %% 2. Edge Detection using Sobel kernels
@@ -31,26 +37,27 @@ grad_scale = GradT*(g_max-g_min);
 %% Seed points selection
 fprintf('Select seed points on original image\n');
 [xi_seed, yi_seed] = getpts();
-%for testing purposes
-%xi_seed = [499,362,496,541]';
-%yi_seed = [523,423,392,440]';
+
 initial_seed = [xi_seed,yi_seed]; %points now organized as [x1, y1; x2 y2; ...] 
 
-%apparently convhull requires input as double
-%k1 = boundary(initial_seed);
+%generate both so boundary or hull can be used
+k1 = boundary(initial_seed);
 k1h = convhull(initial_seed);
-%plot(initial_seed(k1,1), initial_seed(k1,2),'g');
-% plot(initial_seed(k1h,1), initial_seed(k1h,2),'b','LineWidth',2);
+
 %% Pre-plotting
 close all
 imshow(original, []);
 hold on
+
+%For entire chest cavity (convex region)
 plot(initial_seed(k1h,1), initial_seed(k1h,2),'b','LineWidth',2);
 
+%For left lung (non-convex region) (Q4)
+%plot(initial_seed(k1,1), initial_seed(k1,2),'g','LineWidth',2);
 
 %% Snake Initialization
 snake = initial_seed;
-k = 51;
+k = 11;% Another tuning parameter. 11 Is quick, and seems to be somewhat OK
 range = floor(k/2);
 
 energy = zeros(k,k, 3);
@@ -58,19 +65,13 @@ energy = zeros(k,k, 3);
 %(:,:,2) - curvature
 %(:,:,3) - gradient
 
-%% set parameters
-alpha = 0.1;
-gamma = 1;
-beta = 0.00;
-GradT        = 1;%Gradient Threshold
-grad_scale = GradT*(g_max-g_min);
 
 %% 3. Optimization Loop %%
 
 for iteration = 1:max_itr
     %for each point in the snake
     for point_index = 1:size(snake,1)
-        %round to nearest int since fractional pixels don't really exist
+        %round to nearest int since fractional pixels don't exist
         point = round(snake(point_index,:));
                
         %for each point in the kxk neighbourhood
@@ -87,7 +88,7 @@ for iteration = 1:max_itr
                 
                 modsnake = snake;
                 modsnake(point_index,:) = modified_point;
-                %do the elements of the snake need to be re-ordered?
+                
                 energy(i,j,1) = average_distance(modsnake);
                 energy(i,j,2) = curvature(modsnake);
                 
@@ -112,15 +113,16 @@ for iteration = 1:max_itr
         
         %calculate the total energy
         etotal = alpha*energy(:,:,1) + beta*energy(:,:,2) - gamma*energy(:,:,3);
-        %if multiple minimum values are found, the first point is selected
-%         [min_e,idx] = min(etotal, [],'all','linear');      
-%         [x,y] = ind2sub(size(etotal),idx);
         
-        %if there are multiple minimum values, take the one in the middle
+        %if multiple minimum values are found, the first point is selected
+        %This leads to corner snakes
+        %[min_e,idx] = min(etotal, [],'all','linear');      
+        %[x,y] = ind2sub(size(etotal),idx);
+        
+        %if there are multiple minimum values, choose one randomly
         %results in gradient not moving very aggressively
         mins = find(etotal(:)== min(etotal, [],'all'));
         m = mins(randperm(size(mins,1),1));
-%         [x,y] = ind2sub(size(etotal), median(mins));
         %choose the minimum value randomly
         [x,y] = ind2sub(size(etotal), m);
         
@@ -137,13 +139,16 @@ end
 snake;
 energy;
 etotal;
-%k2 = convhull(snake);
-k2 = boundary(snake);
-plot(snake(k2,1),snake(k2,2),'m');
-k2h = convhull(snake);
 
-plot(snake(k2h,1),snake(k2h,2),'c', 'LineWidth', 2);
+% For convex shapes use: 
+kh2 = convhull(snake);
+plot(snake(kh2,1),snake(kh2,2),'c', 'LineWidth', 2);
 
+%for Question four
+%Seems to work better, otherwise boundary is too tight/too loose
+%plot(snake(:,1),snake(:,2),'m', 'LineWidth', 2);
+%k2 = boundary(snake);
+%plot(snake(k2,1),snake(k2,2),'m', 'LineWidth', 2);
 
 %% Test Functions
 
@@ -158,10 +163,6 @@ all = find(test(:)== min(test,[],'all'));
 m = all(randperm(size(all,1),1));
 
 [x,y] = ind2sub(size(test),median(all));
-%test(x,y)
-
-%test with gamma = 0
-% snake should shrink to single point
 
 %% Energy Functions
 
@@ -171,7 +172,6 @@ function [a_dist] = average_distance(snake)
     %slides show distance as norm squared
 
     %wrap back around to the first element of the snake
-    %distance = norm(snake(1,:)-snake(size(snake,1),:))^2;
     distance = norm(snake(1,:)-snake(size(snake,1),:));
     
     for vi = 2:size(snake,1)
@@ -193,6 +193,3 @@ function [curv] = curvature(snake)
     
 end
 
-function [min_e, idx] = minimum_energy(energy)
-
-end

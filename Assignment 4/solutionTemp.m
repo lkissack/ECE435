f533b70c76c0cec6899d99457a7a5e2fe856a4c0
Laura Kissack <lkissack@uvic.ca>
2020-03-10 19:53:48 -0700

    start assignment 4

---
+++ Assignment 4/solutionTemp.m
@@ -0,0 +1,156 @@
+clear; clc; close all; 
+
+%% Problem 1: Read the labels and separate the data accordingly (saving images as
+%.png). In the end, you should have three folder (F,G,D) containing the
+%.png files.
+% hint: functions readtable, split
+%% Create folders
+mkdir('F');
+mkdir('G');
+mkdir('D');
+
+%% Read Table and sort images
+filename = 'labels.txt';
+%can't use space as delimiter since there are different numbers of cols
+T = readtable(filename, 'TextType', 'string');
+
+images = size(T,1)
+
+for index = 1:images
+    %read the row until the second label
+    image = split(T{index,1})
+    source = image(1) + ".pgm";
+    destination = "";
+    if image(2)=="D"
+        %put the image in file D
+        destination = "/D";
+    elseif image(2)=="F"
+        %put the image in file F
+        destination = "/F";
+    elseif image(2)=="G"
+        %put the image in file G
+        destination = "/G";
+    end
+    %check that the file hasn't been moved already
+    %deals with duplicates in the .txt file
+    if isfile(source)
+        movefile(source, destination);
+    end
+end
+
+%% Problem 2: Pre-process the data
+% % pre-process all the data that is in the D, F, G folders (resize, turn to 3-channel
+% % and do histogram eq.). See end of this script for a template of this function. 
+% 
+% % Problem 3: Load all the data in a datastore. The categorical labels are going to be
+% % created based on the names of the folders each sample is in. hint:
+% % imageDatastore function. 
+% 
+% imds = %TO-DO
+% 
+% % show how many samples from each class there are. Ideally, the number of
+% % samples per class should be approximately the same. hint: countEachLabel
+% % function. 
+% 
+% % Randomly split the image datastore into training and testing  
+% train_percent=0.7;
+% [imdsTrain,imdsTest]= %TO-DO
+% 
+% % Problem 4
+% 
+% %extract HoG features
+% cellSize = [4 4];
+% [trainFeatures, trainLabels] = extractHOG(imdsTrain, cellSize); 
+% 
+% % Now extract the features for the test set
+% % TO-DO
+% 
+% %Problem 5: Train an SVM classifier based on the HoG features (hint: fitcecoc function)
+% disp('Training the SVM classifier...');
+% SVMclassifier = %TO-DO
+% 
+% disp('Testing the SVM classifier...');
+% % Make class predictions using the test features. hint(predict function)
+% predictedLabels = %TO-DO
+% 
+% % plot a confusion matrix to analyze the results
+% %TO-DO
+% title('Confusion Matrix for the SVM classifier using HoG features');
+% 
+% %Problem 6: Split the training set into training and validation. That will help
+% % monito the progress of the training process. 
+% 
+% valid_percent=0.3;
+% [imdsValid,imdsTrain]=%TO-DO
+% 
+% % check the distribution of samples left for the training
+% disp('Distribution of samples per class in the training set:');
+% %TO-DO
+% 
+% % create and apply image augmentation structure that randomly reflects the
+% % training images along the x-axis. hint: imageDataAugmenter and
+% % augmentedImageDatastore functions
+% 
+% imageAugmenter = imageDataAugmenter(%TO-DO);
+% augimdsTrain = %TO-DO 
+% 
+% %Problem 7: create a ResNet-50
+% disp('Creating a resnet50 network.')
+% net = %TO-DO; 
+% 
+% % replace the last fully-connected and classification layers to account for
+% % the number of classes (3)
+% %TO-DO
+% 
+% options=trainingOptions('adam', ...
+%     'MiniBatchSize',20, ... % trains on 20 samples at a time
+%     'MaxEpochs',5, ... % passes through the whole dataset 5 times while training
+%     'Shuffle','every-epoch', ... % shuffles the data on every new pass through the dataset 
+%     'InitialLearnRate',1e-4, ... % defines how fast the parameters will change to reduce the loss
+%     'ValidationData',imdsValid, ... % uses validation set to evaluate the network while it is trained
+%     'ValidationFrequency',1, ... % performs the validation every "minibatch" number of samples
+%     'Verbose',false, ... % does not show display training information on the command window
+%     'Plots','training-progress'); % plots the training progress (with validation)
+% 
+% % Train the ResNet-50 network
+% disp('Training the ResNet-50 classifier...');
+% %TO-DO
+% 
+% % Use the trained classifier in the test set 
+% %TO-DO
+% 
+% % Plot a confusion matrix to analyze the results
+% %TO-DO
+% title('Confusion matrix for the ResNet-based classifier');
+% 
+% function [out] = preProcess(img, targetSize)
+% I=img;
+% 
+% % 1.resize the image to 224x224 (targetSize)
+% %RESIZE
+% 
+% %2. Turn the image to three-channel
+% % TURN 3-CHANNEL
+% 
+% %3 apply hist. eq. (hint: histeq function)
+% 
+% out = result;
+% 
+% end
+% 
+% function [features, setLabels] = extractHOG(imds, cellSize)
+% 
+% setLabels = imds.Labels;
+% numImages = numel(imds.Files);
+% 
+% %pre-allocate an array to hold the features (determine its size, hogSize, by 
+% %extracting the HoG from one image with cellSize
+% 
+% features  = zeros(numImages, length(hogSize), 'single'); 
+% 
+% % Extract the features from each image
+% for j = 1:numImages
+%     %TO-DO
+% end
+% end
+
